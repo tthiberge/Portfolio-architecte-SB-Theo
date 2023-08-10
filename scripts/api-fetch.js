d8e0e3273e0116e028e239170b26c177549ca7f4
Theophile Thiberge <tthiberge@gmail.com>
2023-08-10 17:00:17 +0200

    Filters are in place working and style is dynamic

--- scripts/api-fetch.js
+++ scripts/api-fetch.js
@@ -7,10 +7,10 @@ export async function getWorksData() {
 
   if (worksData === null) {
     // Dans ce cas, appel à l'API
-    console.log("Le localStorage était vide, il a fallu fetcher l'API");
-    
-    const response = await fetch("http://localhost:5678/api/works")
-    worksData = await response.json()
+    console.log("Le localStorage des travaux était vide, il a fallu fetcher l'API");
+
+    const responseWorks = await fetch("http://localhost:5678/api/works")
+    worksData = await responseWorks.json()
     console.log(worksData);
 
     // Une fois mis à jour, j'enregistre quand même dans le localStorage
@@ -30,3 +30,32 @@ export async function getWorksData() {
   }
 }
 // => ça a l'air de fonctionner car dans la console, je n'ai plus le console.log du if
+
+
+export async function getCategoriesData() {
+  let categoriesData = window.localStorage.getItem("categories")
+
+  if (categoriesData === null) {
+    // Dans ce cas, appel à l'API
+    console.log("Le localStorage des catégories était vide, il a fallu fetcher l'API");
+
+    const responseCategories = await fetch("http://localhost:5678/api/categories")
+    const categoriesData = await responseCategories.json()
+    console.log(categoriesData);
+
+    // Une fois mis à jour, j'enregistre quand même dans le localStorage
+    // Transformation des catégories en JSON
+    const categories = JSON.stringify(categoriesData)
+    // Stockage des informations dans le localStorage
+    window.localStorage.setItem("categories", categories)
+
+    return categoriesData
+
+  } else {
+    // S'il y a quelque chose dans le localStorage, je le rends lisible
+    console.log("J'ai trouvé qqch dans le localStorage pour les Catégories, je n'ai pas appelé l'API");
+
+    categoriesData = JSON.parse(categoriesData)
+    return categoriesData
+  }
+}
